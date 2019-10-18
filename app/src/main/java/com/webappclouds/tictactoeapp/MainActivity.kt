7796b7062dc4282cea57652ba59b687fbc71f407
Dan Boehmke <Daniel@webappclouds.com>
2019-10-18 13:41:04 -0400

    Game logic

--- app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
@@ -29,7 +29,103 @@ class MainActivity : AppCompatActivity() {
             R.id.but8 -> cellId = 8
             R.id.but9 -> cellId = 9
         }
-        Toast.makeText(this, "Button Pressed: $cellId", Toast.LENGTH_LONG).show()
+//        Toast.makeText(this, "Button Pressed: $cellId", Toast.LENGTH_LONG).show()
 
+        playGame(cellId,butSelected)
+    }
+
+    var activePlayer = 1
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+    fun playGame(cellId: Int, butSelect:Button){
+
+        if (activePlayer == 1) {
+            butSelect.text = "X"
+            butSelect.setBackgroundResource(R.color.blue)
+            player1.add(cellId)
+            activePlayer = 2
+        } else {
+            butSelect.text = "O"
+            butSelect.setBackgroundResource(R.color.darkGreen)
+            player2.add(cellId)
+            activePlayer = 1
+        }
+
+        butSelect.isEnabled = false
+
+        checkWinner()
+    }
+
+    fun checkWinner(){
+        var winner = -1
+
+        // Player 1 Rows
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+            winner = 1
+        }
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            winner = 1
+        }
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            winner = 1
+        }
+        // Player 1 Columns
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            winner = 1
+        }
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            winner = 1
+        }
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            winner = 1
+        }
+        // Player 1 X's
+        if(player1.contains(1) && player1.contains(5) && player1.contains(9)){
+            winner = 1
+        }
+        if(player1.contains(3) && player1.contains(5) && player1.contains(6)){
+            winner = 1
+        }
+
+
+        // Player 2 Rows
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            winner = 2
+        }
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            winner = 2
+        }
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            winner = 2
+        }
+        // Player 2 Columns
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            winner = 2
+        }
+        if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
+            winner = 2
+        }
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            winner = 2
+        }
+        // Player 2 X's
+        if(player2.contains(1) && player2.contains(5) && player2.contains(9)){
+            winner = 2
+        }
+        if(player2.contains(3) && player2.contains(5) && player2.contains(6)){
+            winner = 2
+        }
+
+        if(winner == 1){
+
+            Toast.makeText(this,"Player 1 is the winner", Toast.LENGTH_LONG).show()
+
+        } else if (winner == 2){
+
+            Toast.makeText(this,"Player 2 is the winner", Toast.LENGTH_LONG).show()
+
+        }
     }
 }
