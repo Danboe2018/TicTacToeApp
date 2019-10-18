7d70be50a2c48aea709284326f1a2e2d72286756
Dan Boehmke <Daniel@webappclouds.com>
2019-10-18 13:53:10 -0400

    Added dumb player

--- app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
@@ -5,6 +5,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun butClick(view: View){
+    fun butClick(view: View) {
 
         val butSelected = view as Button
 
         var cellId = 0;
-        when(butSelected.id){
+        when (butSelected.id) {
             R.id.but1 -> cellId = 1
             R.id.but2 -> cellId = 2
             R.id.but3 -> cellId = 3
@@ -31,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
 //        Toast.makeText(this, "Button Pressed: $cellId", Toast.LENGTH_LONG).show()
 
-        playGame(cellId,butSelected)
+        playGame(cellId, butSelected)
     }
 
     var activePlayer = 1
@@ -39,13 +42,14 @@ class MainActivity : AppCompatActivity() {
     var player1 = ArrayList<Int>()
     var player2 = ArrayList<Int>()
 
-    fun playGame(cellId: Int, butSelect:Button){
+    fun playGame(cellId: Int, butSelect: Button) {
 
         if (activePlayer == 1) {
             butSelect.text = "X"
             butSelect.setBackgroundResource(R.color.blue)
             player1.add(cellId)
             activePlayer = 2
+            autoPlay()
         } else {
             butSelect.text = "O"
             butSelect.setBackgroundResource(R.color.darkGreen)
@@ -58,74 +62,106 @@ class MainActivity : AppCompatActivity() {
         checkWinner()
     }
 
-    fun checkWinner(){
+    fun checkWinner() {
         var winner = -1
 
         // Player 1 Rows
-        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
             winner = 1
         }
-        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
             winner = 1
         }
-        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
             winner = 1
         }
         // Player 1 Columns
-        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
             winner = 1
         }
-        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
             winner = 1
         }
-        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
             winner = 1
         }
         // Player 1 X's
-        if(player1.contains(1) && player1.contains(5) && player1.contains(9)){
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
             winner = 1
         }
-        if(player1.contains(3) && player1.contains(5) && player1.contains(6)){
+        if (player1.contains(3) && player1.contains(5) && player1.contains(6)) {
             winner = 1
         }
 
 
         // Player 2 Rows
-        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
             winner = 2
         }
-        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
             winner = 2
         }
-        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
             winner = 2
         }
         // Player 2 Columns
-        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
             winner = 2
         }
-        if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
             winner = 2
         }
-        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
             winner = 2
         }
         // Player 2 X's
-        if(player2.contains(1) && player2.contains(5) && player2.contains(9)){
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
             winner = 2
         }
-        if(player2.contains(3) && player2.contains(5) && player2.contains(6)){
+        if (player2.contains(3) && player2.contains(5) && player2.contains(6)) {
             winner = 2
         }
 
-        if(winner == 1){
+        if (winner == 1) {
 
-            Toast.makeText(this,"Player 1 is the winner", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Player 1 is the winner", Toast.LENGTH_LONG).show()
 
-        } else if (winner == 2){
+        } else if (winner == 2) {
 
-            Toast.makeText(this,"Player 2 is the winner", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Player 2 is the winner", Toast.LENGTH_LONG).show()
 
         }
+    }
+
+    private fun autoPlay() {
+        var emptyCells = ArrayList<Int>()
+        for (cellId in 1..9) {
+            if (!(player1.contains(cellId) || player2.contains(cellId))) {
+                emptyCells.add(cellId)
+            }
+        }
+
+        val r = Random()
+        val randIndex = r.nextInt(emptyCells.size - 0) + 0
+        val cellId = emptyCells[randIndex]
+
+        var butSelected: Button?
+        butSelected = when (cellId) {
+            1 -> but1
+            2 -> but2
+            3 -> but3
+            4 -> but4
+            5 -> but5
+            6 -> but6
+            7 -> but7
+            8 -> but8
+            9 -> but9
+            else -> {
+                but1
+            }
+        }
+
+        playGame(cellId,butSelected)
+
     }
 }
