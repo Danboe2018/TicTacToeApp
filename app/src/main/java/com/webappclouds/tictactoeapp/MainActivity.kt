b703e66f5d4c2a536b125a4fbd3873dd93f38b1a
Dan Boehmke <Daniel@webappclouds.com>
2019-10-18 12:51:36 -0400

    Setup buttons

--- app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/tictactoeapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.webappclouds.tictactoeapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun butClick(view: View){
+
+        val butSelected = view as Button
+
+        var cellId = 0;
+        when(butSelected.id){
+            R.id.but1 -> cellId = 1
+            R.id.but2 -> cellId = 2
+            R.id.but3 -> cellId = 3
+            R.id.but4 -> cellId = 4
+            R.id.but5 -> cellId = 5
+            R.id.but6 -> cellId = 6
+            R.id.but7 -> cellId = 7
+            R.id.but8 -> cellId = 8
+            R.id.but9 -> cellId = 9
+        }
+        Toast.makeText(this, "Button Pressed: $cellId", Toast.LENGTH_LONG).show()
+
     }
 }
